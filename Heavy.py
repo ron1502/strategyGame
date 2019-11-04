142630dae8b0cfbd1f8d2e4535beb51d7daf0933
matthartstein <56240574+matthartstein@users.noreply.github.com>
2019-11-04 13:52:03 -0600

    Add files via upload

--- Heavy.py
+++ Heavy.py
@@ -1,11 +1,11 @@
 class Heavy(Project):
-    def __init__():
+    def __init__(self):
         super("heavy.jpg", "Medium", 1, 1, 1, 1, 1, 1, 1, 1)
-        xpos = super.getX()
-        ypos = super.getY()
+        self.xpos = super.getX()
+        self.ypos = super.getY()
 
-    def update(Graphics g):
-        xpos = super.getX()
-        ypos = super.getY()
+    #def update(Graphics g):
+        #xpos = super.getX()
+        #ypos = super.getY()
         # Movement Function
-        super.update(g)
+        #super.update(g)
--- Medium.py
+++ Medium.py
@@ -1,11 +1,11 @@
 class Medium(Project):
-    def __init__():
+    def __init__(self):
         super("medium.jpg", "Medium", 100, 99, 99, 99, 99, 99, 99, 99)
-        xpos = super.getX()
-        ypos = super.getY()
+        self.xpos = super.getX()
+        self.ypos = super.getY()
 
-    def update(Graphics g):
-        xpos = super.getX()
-        ypos = super.getY()
+    #def update(Graphics g):
+        #xpos = super.getX()
+        #ypos = super.getY()
         # Movement Function
-        super.update(g)
+        #super.update(g)
