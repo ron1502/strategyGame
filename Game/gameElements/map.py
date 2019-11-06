f423ce8b7b7c48c041bdceb4abbac420f8581eb3
ron1502 <ruvelasq@uark.edu>
2019-11-06 15:22:30 -0600

    modified:   Game/gameElements/map.py 	renamed:    tile.py -> Game/gameElements/tile.py 	deleted:    map.py

--- Game/gameElements/map.py
+++ Game/gameElements/map.py
@@ -1,6 +1,30 @@
+from tile import *
 
+def enemiesEliminated(self):#example clear condition
+    return self.enemies is 0
+clearSwitcher = {
+    0: enemiesEliminated
+}
+class map:
 
-class map(sprite):
+    
+    
+    def __init__(self, filename):
+        #in final version this will probably load the file at filename and initialize all of this
+        #for testing purposes I set it all manually for now
+        self.height=0
+        self.width=0
+        self.enemies=0
+        self.clearCondition=0
+        self.tiles=[[]]
+        self.turn=1 #it's 1 when player's turn 2 when enemy's turn.
 
-	def render:
-		
+    def isClear(self):
+        func=clearSwitcher.get(self.clearCondition, "nothing")
+        return func(self)
+        
+#a=map("level1.txt")
+#a.enemies=1
+#print(a.isClear())
+#a.enemies-=1
+#print(a.isClear())
