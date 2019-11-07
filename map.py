7dcd2d0a3405f76b15c84cce5078cba6208b28cf
hayes <h94794@gmail.com>
2019-11-07 15:35:38 -0600

    Added Basic Range Table
    Range Table is a mxn table that represents what tiles a unit is allowed to move to on the map from its present location. It will be used in various logic.
    Presently does not allow friendly units to move through one another.

--- map.py
+++ map.py
@@ -12,18 +12,69 @@ class map:
     def __init__(self, filename):
         #in final version this will probably load the file at filename and initialize all of this
         #for testing purposes I set it all manually for now
-        self.height=0
-        self.width=0
+        self.height=5
+        self.width=5
         self.enemies=0
         self.clearCondition=0
         self.tiles=[[]]
         self.turn=1 #it's 1 when player's turn 2 when enemy's turn.
+        self.rangeTable=[0]*self.width
+        for i in range(self.width):
+            self.rangeTable[i]=[0]*self.height
+        #print(self.rangeTable)
 
     def isClear(self):
         func=clearSwitcher.get(self.clearCondition, "nothing")
         return func(self)
+
+    #this will recursively set up a table representing what tiles a unit may move into
+    #a 1 means that unit can move there a 2 means that unit cannot move there
+    def constructRangeTable(self, x, y, MOV): #x refers to what row the unit is in y is what column it's in.
+        self.rangeTable[x][y]=1 #obviously a unit is allowed to stay where it is, "move" to its own location.
+        if x-1>=0:
+            if self.tiles[x-1][y].isInRange(MOV):
+                nMOV=MOV-self.tiles[x-1][y].type[1]
+                self.constructRangeTable(x-1, y, nMOV)
+        if x+1<self.height:
+            if self.tiles[x+1][y].isInRange(MOV):
+                nMOV=MOV-self.tiles[x+1][y].type[1]
+                self.constructRangeTable(x+1, y, nMOV)
+        if y-1>=0:
+            if self.tiles[x][y-1].isInRange(MOV):
+                nMOV=MOV-self.tiles[x][y-1].type[1]
+                self.constructRangeTable(x, y-1, nMOV)
+        if y+1<self.width:
+            if self.tiles[x][y+1].isInRange(MOV):
+                nMOV=MOV-self.tiles[x][y+1].type[1]
+                self.constructRangeTable(x, y+1, nMOV)
+
+    def clearRangeTable(self):
+        self.rangeTable=[0]*self.width
+        for i in range(self.width):
+            self.rangeTable[i]=[0]*self.height
+            
         
-#a=map("level1.txt")
+a=map("level1.txt") #note that you have to manually set the height and width in the constructor right now
+#if you don't do this rangeTable will not be correctly declared in the initializor
+#this will not be a problem in the final version as height and width will be read from a file before setting up rangeTable
+'''a.height=5
+a.width=5
+a.tiles=[[tile(1), tile(1), tile(1), tile(1), tile(1)],
+         [tile(1), tile(1), tile(1), tile(1), tile(1)],
+         [tile(1), tile(1), tile(1), tile(1), tile(1)],
+         [tile(1), tile(1), tile(1), tile(1), tile(1)],
+         [tile(1), tile(1), tile(1), tile(1), tile(1)]]
+a.constructRangeTable(2, 2, 2)#A unit with 2 movement starting from the middle tile
+for i in range(5):
+    print(a.rangeTable[i])
+
+a.tiles[2][3].unit=10
+a.clearRangeTable()
+a.constructRangeTable(2, 2, 2) #the same move but now a unit is to the right of our unit.
+print("")
+for i in range(5):
+    print(a.rangeTable[i])
+'''
 #a.enemies=1
 #print(a.isClear())
 #a.enemies-=1
--- tile.py
+++ tile.py
@@ -1,4 +1,4 @@
-
+from unit import *
 #for each type: [0]=their sprite [1]=movement cost of tile [2]=defense value of tile [3]=avoidance value of tile
 typeBridge=[" ", 1, 0, -5]
 typePlains=[" ", 1, 0, 0]
@@ -18,12 +18,19 @@ class tile:
     def __init__(self, tType):
         self.type=typeSwitcher.get(tType, "nothing")
         self.unit=None #equivalent to NULL. Refers to what unit is in the tile
+        self.inRange=0
 
     def tileEmpty(self):
         if self.unit is None:
-            return true
+            return True
         else:
-            return false
+            return False
+
+    def isInRange(self, rMov):
+        if(rMov-self.type[1]>=0 and self.tileEmpty()):
+            return True
+        else:
+            return False
 
 
 #a1=tile(2)
