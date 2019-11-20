af42040f220ec02af1071ff64f25b3311f252fec
ron1502 <ruvelasq@uark.edu>
2019-11-19 23:39:56 -0600

    Only instance an instance of game is part of the mani loop

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
-from Game.menuElements.Menu import *
 from Game.Game import Game
 from Game.gameElements.sprite import sprite
-import os
+import pygame
 
 def main():
-        # Eliminate Duplicate operations
         sprite.init(1080, 720, "Best game ever")
-
-        print("Initializing game")
         game = Game()
-        game_intro(1080, 720, game.run)
         
         while game.model.run:
                 game.run()
                 sprite.clock.tick(60)
+
+        pygame.quit()
+
 
 main()
