92373abdf03c6d99b2146f656f0f3e3690dd54b7
darknegan <50059871+darknegan@users.noreply.github.com>
2019-12-09 18:24:45 -0600

    delete unused menu stuff

--- Game/gameElements/menu.py
+++ Game/gameElements/menu.py
@@ -2,22 +2,15 @@
 from Game.gameElements.button import button
 from Game.gameElements.sprite import sprite
 import pygame
-import os
 
 class menu:
     LIGHTGREY = pygame.Color(190, 193, 198)
     BLACK = pygame.Color(0,0,0)
     WHITE = pygame.Color(255,255,255)
     DARKGREY = pygame.Color(77, 77, 77)
-
-    #title = pygame.image.load(path.join(img_dir, "The_Lonely_Shooter.png")).convert_alpha()
-    #title = pygame.transform.scale(title, (WINDOWWIDTH, 81 * 2))
     
-
     def __init__(self):
         self.action = None
-        #self.background = os.getcwd() + (r"\resources\sprites\menu\background.png")
-        #self.background = pygame.image.load(self.background).convert_alpha()
         self.go = button(465, 150, 150, "S", "Start Game", self.goFunct, menu.LIGHTGREY)
         self.quit = button(465, 250, 150, "S", "Quit", self.quitFunct, menu.LIGHTGREY)
         
